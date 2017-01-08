0f5744a7c1931c746f79739ff6cb05548ca92be6
Maga Abdurakhmanov <maqdev@gmail.com>
2017-01-08 01:57:38 +0100

    + build for scala 2.10

--- travis/build.sh
+++ travis/build.sh
@@ -8,8 +8,16 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
   if grep "version\s*:=.*SNAPSHOT" build.sbt; then
     sbt ++$TRAVIS_SCALA_VERSION test publishSigned
   else
-    sbt ++$TRAVIS_SCALA_VERSION test publishSigned sonatypeReleaseAll
+    sbt ++$TRAVIS_SCALA_VERSION test publishSigned
+  	# wait different time for different jobs, due to race condition releasing in sonatype
+  	if [[ "$TRAVIS_JOB_NUMBER" =~ ^[[:digit:]]+\.([[:digit:]]+)$ ]]; then
+  		job_number=${BASH_REMATCH[1]}
+  		wait_time=$((($job_number-1)*20))
+  		echo "Waiting for job $job_number ($TRAVIS_JOB_NUMBER) for $wait_time seconds..."
+  		sleep $wait_time
+  	fi
+    sbt ++$TRAVIS_SCALA_VERSION sonatypeReleaseAll
   fi
 else
-  sbt +test
+  sbt ++$TRAVIS_SCALA_VERSION test
 fi
