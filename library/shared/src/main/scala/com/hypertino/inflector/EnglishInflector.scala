44736bd4f0421d80595f637f0c76155cabb06823
Ethan Eldridge <Ethan.Eldridge@meredith.com>
2018-01-19 14:53:37 -0500

    Fix Issue #6

--- library/shared/src/main/scala/com/hypertino/inflector/EnglishInflector.scala
+++ library/shared/src/main/scala/com/hypertino/inflector/EnglishInflector.scala
@@ -37,7 +37,7 @@ class EnglishInflector(val anglicizedEnglish: Boolean) extends TwoFormInflector 
       "tuna", "djinn", "mumps", "whiting", "eland", "news", "wildebeest", "elk", "pincers", "sugar") ++
     irregular(("child", "children"), ("ephemeris", "ephemerides"), ("mongoose", "mongoose"),
       ("mythos", "mythoi"), ("soliloquy", "soliloquies"), ("trilby", "trilbys"), ("genus", "genera"),
-      ("quiz", "quizzes"), ("basis", "bases"), ("slice", "slices"), ("cookie", "cookies"), ("response", "responses")) ++ {
+      ("quiz", "quizzes"), ("basis", "bases"), ("slice", "slices"), ("cookie", "cookies"), ("response", "responses"), ("case", "cases")) ++ {
       if (anglicizedEnglish) {
         irregular(("beef", "beefs"), ("brother", "brothers"), ("cow", "cows"),
           ("genie", "genies"), ("money", "moneys"), ("octopus", "octopuses"), ("opus", "opuses"))
